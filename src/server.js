ae24d6ea05d57078c98006e96d7f4f19fe4c2820
Algorithmic and Programming Society, NITR <apsociety.nitr@gmail.com>
2022-11-08 16:11:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,9 +6,7 @@ const cors = require('cors') // includes cors module
 
 require('dotenv').config()
 
-app.use(cors({
-    origin: '*'
-})) // We're telling express to use CORS
+app.use(cors()) // We're telling express to use CORS
 app.use(express.json()) // we need to tell server to use json as well
 app.use(routes) // tells the server to use the routes in routes.js
 
