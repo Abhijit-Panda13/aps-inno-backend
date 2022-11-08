97f2fb22c482031e8128110b57fdd9150fdda94a
Algorithmic and Programming Society, NITR <apsociety.nitr@gmail.com>
2022-11-08 17:39:30 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,20 @@ const cors = require('cors') // includes cors module
 
 require('dotenv').config()
 
+const whitelist = ["http://localhost:3000/", "https://the-timeless-saga.netlify.app/"]
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 app.use(cors()) // We're telling express to use CORS
 app.use(express.json()) // we need to tell server to use json as well
 app.use(routes) // tells the server to use the routes in routes.js
