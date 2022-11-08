5fa5dc714962280d4f00d2d24b133cbf35682763
Algorithmic and Programming Society, NITR <apsociety.nitr@gmail.com>
2022-11-08 16:55:19 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,18 +2,24 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const routes = require('./routes') // includes the routes.js file
-//const cors = require('cors') // includes cors module
+const cors = require('cors') // includes cors module
 
 require('dotenv').config()
 
-//app.use(cors()) // We're telling express to use CORS
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts)) // We're telling express to use CORS
 app.use(express.json()) // we need to tell server to use json as well
 app.use(routes) // tells the server to use the routes in routes.js
 
